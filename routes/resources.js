5ad3ce02adeef871e6d4cc9d053d0f91a33a9b6c
alex-blair <alex.n.blair@gmail.com>
2017-03-23 10:45:09 +1300

    add routes to server

---
+++ routes/resources.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../db/db')
+
+router.get('/', function (req, res) {
+  db.getResources()
+  .then(function (result) {
+    console.log(result)
+    res.send(result)
+  })
+})
+
+module.exports = router
