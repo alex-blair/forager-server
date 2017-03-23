a8f4c4e7a122094e72f32c860e1332fdf4959407
alex-blair <alex.n.blair@gmail.com>
2017-03-23 14:19:51 +1300

    add postgres module

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-// const path = require('path')
+const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const server = express()
 
 const resources = require('./routes/resources')
+
+server.use(bodyParser.json())
+server.use(express.static('public'))
+// server.use(express.static(path.join(__dirname, './public')))
 
 server.use('/resources', resources)
 
