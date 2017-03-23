66fc6772ff65bb65fe9c58597b0129d7c79a79f0
alex-blair <alex.n.blair@gmail.com>
2017-03-23 16:30:40 +1300

    edit typo of double minus in data value

--- seeds/resources.js
+++ seeds/resources.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('resources').insert([
         {id: 1, type: 'Apple Tree', lat:'-36.886900', long:'174.760750', timestamp:'1490220611', notes:'Lovely sour apples'},
-        {id: 2, type: 'Lemon Tree', lat:'--36.862365', long:'174.761512', timestamp:'1490220611', notes:'Kind of hard to get too, but lots of lemons!'},
+        {id: 2, type: 'Lemon Tree', lat:'-36.862365', long:'174.761512', timestamp:'1490220611', notes:'Kind of hard to get too, but lots of lemons!'},
         {id: 3, type: 'Feijoa Tree', lat:'-36.867336', long:'174.759657', timestamp:'1490220611', notes:'A huge tree overhanging a fence'}
       ])
     })
