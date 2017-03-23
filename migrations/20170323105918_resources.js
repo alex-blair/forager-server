d0aa79674b72ed0a12199bc8357138895bd1835d
alex-blair <alex.n.blair@gmail.com>
2017-03-23 16:21:06 +1300

    fix decimal values in data migration

--- migrations/20170323105918_resources.js
+++ migrations/20170323105918_resources.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('resources', function (table) {
     table.increments('id').primary()
     table.string('type')
-    table.decimal(18,8)('lat')
-    table.decimal(18,8)('long')
+    table.decimal('lat', 18, 8)
+    table.decimal('long', 18, 8)
     table.integer('timestamp')
     table.string('notes')
   })
