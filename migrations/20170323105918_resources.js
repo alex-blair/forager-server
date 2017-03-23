86ef057297cc9ad02fea717fda5566db19d1c906
alex-blair <alex.n.blair@gmail.com>
2017-03-23 16:14:14 +1300

    extend scale of decimal value in data table to 8 decimal places

--- migrations/20170323105918_resources.js
+++ migrations/20170323105918_resources.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('resources', function (table) {
     table.increments('id').primary()
     table.string('type')
-    table.decimal('lat')
-    table.decimal('long')
+    table.decimal(18,8)('lat')
+    table.decimal(18,8)('long')
     table.integer('timestamp')
     table.string('notes')
   })
