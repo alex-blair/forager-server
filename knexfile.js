97e90697ac281b2d2832bd3b686cdfd8c4687ca3
alex-blair <alex.n.blair@gmail.com>
2017-05-26 12:28:46 +1200

    update knex file

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,18 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    useNullAsDefault: true
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
