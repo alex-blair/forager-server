bac490645a8975f4884b5f5acc5636d1de8a0ea0
alex-blair <alex.n.blair@gmail.com>
2017-05-29 10:18:32 +1200

    test graphql

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const server = express()
 
 const resources = require('./routes/resources')
+
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+let root = {
+  hello: () => {
+    return 'Hello world!'
+  }
+}
+
+server.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+server.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
 
 server.use(bodyParser.json())
 server.use(express.static('public'))
