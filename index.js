6e8cb8f981da8325238f2cf94635ee65513636e9
alex-blair <alex.n.blair@gmail.com>
2017-06-07 16:16:25 +0200

    fix typo in index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var server = require('./server')
 var PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
-  console.log('GraphQL Server is now runnino on', PORT)
+  console.log('GraphQL Server is now running on', PORT)
 })
